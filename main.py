2ce63e3653df52d4163f16e20b665c28689db457
Crisgrva <crstngranada@gmail.com>
2022-01-18 03:57:15 +0000

    [ Main ] Validations_path [ 01/18/2022/03:57 ]

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Python
+from lib2to3.pgen2.token import TILDE
 from lib2to3.pytree import Base
+from turtle import title
 from typing import Optional
 
 # Pydantic
@@ -7,7 +9,7 @@ from pydantic import BaseModel
 
 # FastApi
 from fastapi import FastAPI
-from fastapi import Body, Query
+from fastapi import Body, Query, Path
 
 
 app = FastAPI()
@@ -37,7 +39,30 @@ def create_person(person: Person = Body(...)):
 # Validations: query parameters
 @app.get("/person/detail")
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50),
-    age: str = Query(...)
+    name: Optional[str] = Query(
+        None,
+        min_length=1,
+        max_length=50,
+        title="Person name",
+        description="This is the person name. It's between 1 and 50 characteres"
+    ),
+    age: str = Query(
+        ...,
+        title="Person Age",
+        description="This is the person age. It's required"
+    )
 ):
     return {name: age}
+
+
+# Validations: Path parameters
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title="Person id",
+        description="This is the person id. It's required",
+    )
+):
+    return {person_id: "It exists!"}
