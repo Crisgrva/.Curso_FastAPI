bcf4a45962323eebf9a2addfabd59756efd2a08b
Crisgrva <crstngranada@gmail.com>
2022-01-18 03:30:12 +0000

    [ Main ] Request_response_body [ 01/18/2022/03:30 ]

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
+# Python
+from lib2to3.pytree import Base
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastApi
 from fastapi import FastAPI
+from fastapi import Body
 
 
 app = FastAPI()
 
 
+# Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
+
 @app.get("/")
 def home():
     return {"Hello": "World"}
+
+
+# Requests and Response Body
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
